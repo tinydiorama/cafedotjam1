0c8568997032bd502b86fbe63fae8e79ca2694ca
lunsh <errornames@gmail.com>
2023-11-17 08:11:08 -0500

    Initial game & title screen

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject creditsPanel;
+
+    public LevelLoader loader;
+
+    private void Awake()
+    {
+        loader = LevelLoader.instance;
+    }
+
+    public void startGame()
+    {
+        loader.LoadLevel((int)SceneIndexes.MAIN_WORLD, new Vector2(0f, 0f));
+    }
+    public void showCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void hideCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+}
